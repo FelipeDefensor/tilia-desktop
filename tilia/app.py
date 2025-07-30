223a9c8819c99619a1e32a117a4fa590cef97bdb
Felipe Martins <felipe.martins4@hotmail.com>
2025-07-30 10:47:19 -0300

    phd: keep main window on top when opening PDF or video window

--- tilia/app.py
+++ tilia/app.py
@@ -244,6 +244,8 @@ class App:
             self.player, path, initial_duration=initial_duration
         )
 
+        get(Get.MAIN_WINDOW).activateWindow()
+
         self.player = player
 
         if success and record:
